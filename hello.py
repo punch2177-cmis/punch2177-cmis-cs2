cd767c4ba4e4b66545507b88d1f61310614aceb4
cmis <cmis@cmis-hs20.(none)>
2016-01-27 11:12:41 +0700

    I did stuff

---
+++ hello.py
@@ -0,0 +1,32 @@
+
+print "Hello, World"
+print "I like ice cream"
+print "My name is Punch"
+print "Sadness and Disgust are friends"
+print "I am Sadness"
+print "Curi is disgust"
+print "I scream for ice cream"
+print "You scream for ice cream"
+print "We all scream for ice cream"
+print "Disgust has a warm jacket"
+print "Sadness is not nice, therefore sadness will take it"
+print "Disgust is disgusted by Sadness' actions"
+print "Sadness is sad that disgust is disgusted"
+print "Sadness thinks sadness is sad"
+print "BYE"
+print True
+print False
+print "Heyyyy"
+print "Yo"
+print ("Heyyyy", "Yo")
+print 100
+print 1.9
+print "Sadness"
+print "Awesome"
+print 14
+print ("Sadness", "Awesome", 14)
+print "Apple"
+print "Banana"
+print "Oranges"
+print "Watermelon"
+print ("Apple", "Banana", "Oranges", "Watermelon")
