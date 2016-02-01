af72bc6c912dc862f397f136482ab2b3bfbb57fe
cmis <cmis@cmis-hs20.(none)>
2016-02-01 09:55:22 +0700

    new file

---
+++ assignment1.py
@@ -0,0 +1,26 @@
+myName = "Punch"
+myAge = 14.4
+myHeight = 1.59
+sideOfSquare = 4
+lengthOfRectangle = 6
+heightOfRectangle = 4
+theMonths = 12
+ageInMonths = myAge * theMonths
+print ageInMonths
+lifeExpectancy = 74.19
+leftToLive = lifeExpectancy - myAge
+print leftToLive
+inFeet = 3.28084
+meterToFeet = myHeight * inFeet
+print meterToFeet
+averageHeight = 1.59
+differenceHeight = averageHeight - myHeight
+print differenceHeight
+areaSquare = sideOfSquare * sideOfSquare
+print areaSquare
+volumeSquare = sideOfSquare ** 3
+halfVolume = volumeSquare / 2
+print halfVolume
+areaOfRectangle = lengthOfRectangle * heightOfRectangle
+ninthRectangle = float(areaOfRectangle) / 1/9
+print ninthRectangle
