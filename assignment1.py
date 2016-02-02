cf20d51d820f066e4ca92bc0a8d2ce7a99766d85
cmis <cmis@cmis-hs20.(none)>
2016-02-02 11:49:12 +0700

    I editted and finished the assignment

--- assignment1.py
+++ assignment1.py
@@ -1,26 +1,29 @@
-myName = "Punch"
-myAge = 14.4
-myHeight = 1.59
-sideOfSquare = 4
-lengthOfRectangle = 6
-heightOfRectangle = 4
-theMonths = 12
-ageInMonths = myAge * theMonths
-print ageInMonths
-lifeExpectancy = 74.19
-leftToLive = lifeExpectancy - myAge
-print leftToLive
-inFeet = 3.28084
-meterToFeet = myHeight * inFeet
-print meterToFeet
-averageHeight = 1.59
-differenceHeight = averageHeight - myHeight
-print differenceHeight
-areaSquare = sideOfSquare * sideOfSquare
-print areaSquare
-volumeSquare = sideOfSquare ** 3
-halfVolume = volumeSquare / 2
-print halfVolume
-areaOfRectangle = lengthOfRectangle * heightOfRectangle
-ninthRectangle = float(areaOfRectangle) / 1/9
-print ninthRectangle
+myName = "Punch" #defined my name variable
+myAge = 14.4 #defined my age variable
+myHeight = 1.59 #defined my height variable
+sideOfSquare = 4 #define the side of my square
+lengthOfRectangle = 6 #defined the length of the rectangle
+heightOfRectangle = 4 #define the height of my rectangle
+theMonths = 12 #defined the number of months in a year
+ageInMonths = myAge * theMonths #found my age in months by multiplying
+lifeExpectancy = 74.19 #defined the average life expectancy in Thailand
+leftToLive = lifeExpectancy - myAge #foudn my years left to live by subtracting
+inFeet = 3.28084 #found the number of feet in a meter
+meterToFeet = myHeight * inFeet #converted my height into feet
+averageHeight = 1.59 #found the average height for a thai female
+differenceHeight = averageHeight - myHeight #found the difference between my height and the average height
+areaSquare = sideOfSquare * sideOfSquare #found the area of my square by multiplying
+volumeSquare = sideOfSquare ** 3 #found the volume of my cube
+halfVolume = volumeSquare / 2 #found half of the volume of my cube
+areaOfRectangle = lengthOfRectangle * heightOfRectangle #found the area of my rectangle
+ninthRectangle = float(areaOfRectangle) * (1.0/9) #found a ninth of the area of my rectangle
+print "My" + "name" + "is" + myName #used the variable myName and printed
+print "I" + "am" + str(myAge) + "years" + "old." #used the variable myAge and printed
+print "I" + "have" + str(leftToLive) + "years" + "left" + "to" + "live" #used the variable leftToLive and printed
+print "I" + "am" + str(myHeight) + "feet" + "tall" #used the variable myHeight and printed
+print "The" + "difference" + "between" + "the" + "average" + "height" + "and" + "mine" + "is" + str(differenceHeight) #used the variable differenceHeight and printed
+print ("Hello.", "The", "side", "of", "my", "square", "is", str(sideOfSquare), "and", "the", "area", "of", "my", "square", "is", str(areaSquare) ) #used the variable sideOfSquare and areaSquare and printed
+print ("The", "length", "of", "my", "rectangle", "is", str(lengthOfRectangle), "and", "the", "height", "is", str(heightOfRectangle) ) #used the variable lengthOfRectangle and heightOfRectangle and printed
+print ("The", "area", "of", "my", "rectangle", "is", str(areaOfRectangle) ) #used the variable areaOfRectangle and printed
+print ";)"*10000 #made 10000 winking smiley faces and printed
+
