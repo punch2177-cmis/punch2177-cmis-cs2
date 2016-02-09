aaa6e37eefe0bb2e37441b86514c842994839885
cmis <cmis@cmis-hs20.(none)>
2016-02-09 14:04:50 +0700

    editted

--- functions.py
+++ functions.py
@@ -1,10 +1,35 @@
 def add(a,b):
-	return (a+b)
-add(1,2)
+	return a+b
+print add(1,2)
 def sub(c,d):
-    return (c-d)
-sub(5,3)
+    return c-d
+print sub(5,3)
 def mul(e,f):
-    return (e*f)
-mul(4,4)
-def 
+    return e*f
+print mul(4,4)
+def div(g,h):
+	return g/h
+print div(2.0,3)
+def hours_from_seconds(seconds):
+    return seconds / 3600
+print hours_from_seconds(86400)
+import math
+def circle_area(radius):
+    return math.pi * radius**2
+print circle_area(5)
+def sphere_volume(radius):
+    return 4.0/3 * math.pi * radius **3
+print sphere_volume(5)
+def avg_volume(a, b):
+    return ((1.0/6 * math.pi * a**3) + (1.0/6 * math.pi * b**3)) / 2
+print avg_volume(10,20)
+def area(a, b, c):
+    s = (a+b+c)/2
+    return (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print area(1,2,2.5)
+def right_align(string):
+    return
+def cetner(string);
+    return
+def msg_box(string):
+    return "|string|"
