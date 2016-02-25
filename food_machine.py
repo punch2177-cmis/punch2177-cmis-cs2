e32395023aa1f430d584c9a6adc34cdbb7edf491
cmis <cmis@cmis-hs20.(none)>
2016-02-25 09:53:48 +0700

    edit

--- food_machine.py
+++ food_machine.py
@@ -1,36 +1,40 @@
-def mul( x, y):
-    return x * y
-def tax(a, b):
-	int(a) * int(b) = c
-    return c + a
-def discount(w,z):
-    100 * z = r
-    return w - r
-def output(name, x, food, y, a):
+#defined functions that are need for the program to work
+def total_price(a, b, c, d):
+    g = float(a) * float(b)
+    h = g * float(c)
+    i = g - h
+    j = i * float(d)
+    k = j + i
+    return k
+
+def output(name, x, food, y, b):
     cost = """
 Welcome our valued customer, {}!
+Today we have a promotion, 15% off your total price!
+Also, just to inform you, our tax rate is 10%. 
 Currently you are ordering {} {}(s), which costs ${} each.
-Today we have a promotion, 15% off each product!
 Your total with tax and discount is ${}
 Please pay at the counter infront. 
 Thank you!
 Please come again! :)
-""".format(name, x, food, y, a)
+""".format(name, x, food, y, b)
     return cost
 
+
+#The main function includes all the functions define above and calls them so the program works
 def main():
     name = raw_input("What is your name?")
     food = raw_input("What is the food you want right now?")
     x = raw_input("Type how much of that food you want: ")
     y = 10.0
-    e = 0.05
+    tax_percent = 0.10
     discount_percent = 0.15
     
-    z = mul(int(x), int(y))
-    b = discount(int(z), int(discount_percent))
-    a = tax(int(b), int(e))
+    b = total_price(float(x), float(y), discount_percent, tax_percent)
     
-    cost = output(name,  x, food, y, a)
+    cost = output(name,  x, food, y, b)
     print cost
 
+
+#calling main function
 main()
