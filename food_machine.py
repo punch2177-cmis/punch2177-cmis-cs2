030e8cf03aa5e50d85976ee6dec26b763c61d16a
cmis <cmis@cmis-hs17.(none)>
2016-03-04 14:24:25 +0700

    Done?

--- food_machine.py
+++ food_machine.py
@@ -1,13 +1,13 @@
 #defined functions that are need for the program to work
-def total_price(a, b, c, d):
-    g = float(a) * float(b)
-    h = g * float(c)
-    i = g - h
-    j = i * float(d)
-    k = j + i
-    return k
+def total_price(pricePerOne, amount, discountRate, taxRate):
+    price = float(pricePerOne) * float(amount)
+    rate = price * float(discountRate)
+    discount = price - rate
+    rate_2 = discount * float(taxRate)
+    total = rate_2 + discount
+    return total
 
-def output(name, x, food, y, b):
+def output(name, amount, food, costPerOne, total):
     cost = """
 Welcome our valued customer, {}!
 Today we have a promotion, 15% off your total price!
@@ -17,7 +17,7 @@ Your total with tax and discount is ${}
 Please pay at the counter infront. 
 Thank you!
 Please come again! :)
-""".format(name, x, food, y, b)
+""".format(name, amount, food, costPerOne, total)
     return cost
 
 
@@ -25,14 +25,14 @@ Please come again! :)
 def main():
     name = raw_input("What is your name?")
     food = raw_input("What is the food you want right now?")
-    x = raw_input("Type how much of that food you want: ")
-    y = 10.0
+    amount = raw_input("Type how much of that food you want: ")
+    costPerOne = 10.0
     tax_percent = 0.10
     discount_percent = 0.15
     
-    b = total_price(float(x), float(y), discount_percent, tax_percent)
+    total = total_price(float(amount), float(costPerOne), discount_percent, tax_percent)
     
-    cost = output(name,  x, food, y, b)
+    cost = output(name, amount, food, costPerOne, total)
     print cost
 
 
