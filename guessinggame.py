81a8a9710565d26e13d2165d8ca44b685f2085f8
cmis <cmis@cmis-hs20.(none)>
2016-03-15 09:55:15 +0700

    ahhh

--- guessinggame.py
+++ guessinggame.py
@@ -1,15 +1,31 @@
-import random
+
 def guess():
+    import random
     number1 = int(raw_input("What is the minimun number?"))
     number2 = int(raw_input("What is the maximum number?"))
     random = random.randint(number1, number2)
     print "I am thinking of a number in between", number1, "to", number2
     think = int(raw_input("What do you think it is?: "))
     if think == random:
-        return True
+        print  """
+The target was {}
+Your guess was {}
+You are correct. You must be psychic!
+""".format(random, think)
+    elif think > random:
+        difference = think - random
+        print """
+The target was {}
+Your guess was {}
+You are over by {}
+""".format(random, think, difference)
     else:
-        return False
-
-def template( 
+        difference = random - think
+    print """
+The target was {}
+Your guess was {}
+You are under by {}
+""".format(random, think, difference)
        
+guess()
 
