38127a63b5f2c9a44574f5b0f690f43ff8c25827
cmis <cmis@cmis-hs07.(none)>
2016-05-06 11:55:03 +0700

    h

--- guess2.py
+++ guess2.py
@@ -1,20 +1,37 @@
 def guess(random, tries):
-    think = int(raw_input("What do you think it is?: "))
-    if think == random:
-        print  """
-You are correct. You must be psychic!
-"""
+    think = int(raw_input("I am thinking of a number between 0 and 100.\nWhat do you think it is?: "))
+    if tries == 0:
+        print "You are not good at this game"
+        return float(0)
     elif think > random:
         print "You are too high"
         guess(random, tries-1)
+        return 0
     elif think < random:
     	print "You are too low"
         guess(random, tries-1)
-       
+        return 0
+    elif think == random:
+        print "You are correct"
+        return 1
+
+def rounds(tries, numberofRounds, correct):
+    import random
+    random = random.randint(0,100)   
+    if numberofRounds == 0:
+        print correct
+    elif numberofRounds != 0:
+        round1 = guess(random, tries)
+        total1 = float(correct) + float(round1)
+        print "\nYou have " + str(numberofRounds - 1) + " rounds left\n"
+        rounds(tries, numberofRounds - 1, total1)
+
+
 def main():
     import random
-    random = random.randint(0, 100)
-    tries = 5
-    a = guess(random, tries)
+    tries = 4
+    numberofRounds = 3
+    correct = 0
+    a = rounds(tries, numberofRounds, correct)
 
 main()
