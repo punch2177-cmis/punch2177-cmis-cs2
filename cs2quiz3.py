f615129637d9f4412a4685ef6e10dfa251e50342
cmis <cmis@cmis-hs07.(none)>
2016-05-18 11:45:21 +0700

    quiz 3

---
+++ cs2quiz3.py
@@ -0,0 +1,73 @@
+#Section 1: Terminology
+# 1) What is a recursive function?
+#A recursive function is a funcation that calls itself within the function
+#
+#
+# 2) What happens if there is no base case defined in a recursive function?
+#The recursive function will keep calling itself and there will be an error.
+#
+#
+# 3) What is the first thing to consider when designing a recursive function?
+#The base case
+#
+#
+# 4) How do we put data into a function call?
+#You assign it in a variable
+#
+# 
+# 5) How do we get data out of a function call?
+#You return or print the data
+#
+#
+
+#Section 2: Reading
+# Read the following function definitions and function calls.
+# Then determine the values of the variables a1-d3.
+
+#a1 = 6
+#a2 = 8
+#a3 = -1
+
+#b1 = 2
+#b2 = 1
+#b3 = 3
+
+#c1 = -2
+#c2 = 4
+#c3 = 45
+
+#d1 = -4
+#d2 = 8
+#d3 = -4
+
+#Section 3: Programming
+#Write a script that asks the user to enter a series of numbers.
+#When the user types in nothing, it should return the average of all the odd numbers
+#that were typed in. 
+#In your code for the script, add a comment labeling the base case  on the line BEFORE the base case.
+#Also add a comment label BEFORE the recursive case.
+#It is NOT NECESSARY to print out a running total with each user input.
+
+
+def average(total, amount):
+    number = raw_input("Next number: ")
+#Base case
+    if number == "" and amount != 0 and total != 0:
+        print amount/total
+    elif number == "" and amount == 0 and total == 0:
+        print " "
+#Recursive case
+    elif float(number) % 2 == 0:
+        average(total, amount)
+#Recursive case
+    else: 
+        average(total + 1, amount + float(number))
+
+
+def main():
+    total = 0
+    amount = 0
+    average(total, amount)
+
+main()
+
